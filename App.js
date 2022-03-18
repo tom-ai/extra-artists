759d71d54156c401da06e697b53431ee6f606f3a
Tom Harris <tomharris3@me.com>
2022-03-18 23:01:22 +0000

    text input set state

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { TextInput, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text style={{ margin: 15 }}>Extra Artists</Text>
+      <TextInput
+        style={{
+          height: 50,
+          backgroundColor: "#FFFF",
+          padding: 10,
+        }}
+        placeholder={"Paste a song ID"}
+        onChangeText={(input) => {
+          setText(input);
+        }}
+        defaultValue={text}
+      />
       <StatusBar style="auto" />
     </View>
   );
