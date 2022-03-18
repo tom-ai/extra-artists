b681fb7bfce0d42725e292c5b77611602780b3ed
Tom Harris <tomharris3@me.com>
2022-03-18 23:23:10 +0000

    retrieve api data with axios

--- App.js
+++ App.js
@@ -2,9 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { TextInput, StyleSheet, Text, View } from "react-native";
 import { useState } from "react";
+import axios from "axios";
 
 export default function App() {
   const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    const songId = e.target.value;
+    axios
+      .get(`https://api.discogs.com/releases/${songId}`)
+      .then(({ data: { title, resource_url, extraartists, artists } }) => {
+        const songObj = {
+          title: title,
+          artist: artists[0].name,
+          extraArtists: extraartists,
+          url: resource_url,
+        };
+        console.log(songObj);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <View style={styles.container}>
       <Text style={{ margin: 15 }}>Extra Artists</Text>
@@ -17,6 +35,9 @@ export default function App() {
         placeholder={"Paste a song ID"}
         onChangeText={(input) => {
           setText(input);
+        }}
+        onSubmitEditing={(e) => {
+          handleSubmit(e);
         }}
         defaultValue={text}
       />
